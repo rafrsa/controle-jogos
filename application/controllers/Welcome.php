f8a8b935760c5ca8049e348f8dfcd28e4bf5e02a
Rafael Frecceiro Santos <rafa.frecceiro@gmail.com>
2017-11-28 15:29:52 -0200

    criação da dashboarad

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,6 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
+    public $layout = 'admin';
+
 	public function index()
 	{
 		$this->load->view('welcome_message');
